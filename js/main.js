529beae254b3abfc82306316f9687cb6d13b9c08
patrikpajo14 <62564221+patrikpajo14@users.noreply.github.com>
2022-01-15 15:56:44 +0100

    Added new files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function hamburgerDrop() {
+    var x = document.getElementById("links");
+    var y = document.getElementById("izlozbe");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+var title = document.getElementById("title");
+
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("gallery-item")) {
+        const src = e.target.getAttribute("src");
+        title.innerHTML = e.target.getAttribute("alt");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
